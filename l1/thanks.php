53cb3e862f7080ac875fb6d7a482e922a3e1a428
Juan Carlos Cárdenas <juancarlos.cardenas@aesir.com.mx>
2021-01-28 12:24:33 -0600

    Creación de repositorio para landings de MAPFRE 2021

---
+++ l1/thanks.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include_once('template/head.php')?>
+</head>
+<body class="bg-general d-flex justify-content-center align-items-center">
+    <section class="row col-12 m-0 p-0 text-center bg-general-mobile-thanks section-height" style="display:flex !important; align-items:center !important">
+        <div class="col-12 h-50">
+            <img class="mx-auto" src="assets/images/ok_g.png" alt="ok">
+            <h1 class="c-white f-bold col-12 col-md-12 col-lg-5 mx-auto txt-title d-mobile">¡Gracias por dejarnos tus datos!</h1>
+            <h1 class="c-white f-bold col-12 col-md-12 col-lg-5 mx-auto txt-title d-desktop mt-4 mb-4">¡Gracias por dejarnos tus datos!</h1>
+            <h5 class="c-white col-12">Uno de nuestros asesores se pondrá en contacto contigo</h5>
+        </div>
+    </section>
+</body>
+</html>
